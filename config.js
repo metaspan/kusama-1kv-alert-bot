4840d7d9e2597b737973da411eec03fe220a719b
Derek Colley <derek@colley.cc>
2022-06-06 18:23:25 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 
 const config = {
-    bot_id: '5859',
-    bot_token: 'OTgzMzQ3Mjk1OTY3OTIwMTY4.G5W_w1._pl8_MgFZmpS5AKbGySfevhViRQARd2tmCFr1c',
-    app_id: '983347295967920168',
-    public_key: 'c14087a8cc32810dc96ffa648f7eec29eaad00344a1ee4372041dbe06e264ca7',
+    bot_token: 'your_bot_token',
+    app_id: 'your_app_id',
+    public_key: 'your_app_public_key',
 }
 export default config
